3521bc9323c0175eac434151fdcbc93460b13daf
Kishore <gladwinvec@gmail.com>
2022-09-02 19:38:48 +0530

    Update user.py

--- user.py
+++ user.py
@@ -32,13 +32,13 @@ class UserFrame_std(Frame):        #STD Frame
     def __init__(self, parent, **args):
         Frame.__init__(self, parent, background="white")
         self.parent = parent
-        self.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15), weight=1, minsize=20)
+        self.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,16), weight=1, minsize=20)
         self.grid_columnconfigure((0, 1, 2,3,4,5,6), weight=1)
 
         #==== VARIABLES =====
 
         # Standard Deviation Variables
-        self.std_labels = ['STD-1','STD-2','STD-3','STD-4','STD-5','STD-6','STD-7','STD-8','STD-9','STD-10','Ambient','Ambient correction','Error of DMM','Error of sensor']
+        self.std_labels = ['STD-1','STD-2','STD-3','STD-4','STD-5','STD-6','STD-7','STD-8','STD-9','STD-10','AVG. STD','Ambient','Ambient correction','Error of DMM','Error of sensor']
         self.std_vars = [DoubleVar()for i in range(len(self.std_labels))]
         
         #====================
@@ -50,6 +50,8 @@ class UserFrame_std(Frame):        #STD Frame
         for i, value in enumerate(self.std_labels):
             ttk.Label(self, text=value, justify='left', padding=(10, 5, 10, 5)).grid(row=i, column=1, sticky='new')
         for i in range(len(self.std_labels)):
+            if i==10:
+                continue
             entry=ttk.Entry(self, textvariable=self.std_vars[i])
             entry.grid(row=i, column=2, padx=10, sticky='ew')
             entry.bind('<Return>', self.entry_next)
@@ -57,7 +59,7 @@ class UserFrame_std(Frame):        #STD Frame
             self.data.append(entry)
 
         self.save_btn = ttk.Button(self, text='Save', width=10, command=self.on_save)
-        self.save_btn.grid(row=14, columnspan=3, padx=10, pady=10, sticky='e')
+        self.save_btn.grid(row=15, columnspan=3, padx=10, pady=10, sticky='e')
         self.save_btn.bind('<Return>', self.on_save)
         
     def on_save(self, e=None):
@@ -70,9 +72,9 @@ class UserFrame_std(Frame):        #STD Frame
             sum=sum+self.std_vars[i].get()
         avg=sum/10
         #print("\n The average is ",avg)
-        true_value_std_c= avg + self.std_vars[11].get() - self.std_vars[12].get()
+        true_value_std_c= avg + self.std_vars[12].get() - self.std_vars[13].get()
         #print("\n The true value in celsius is ",true_value_std_c)                #true value of std in celsius
-        true_value_std=true_value_std_c - self.std_vars[13].get()
+        true_value_std=true_value_std_c - self.std_vars[14].get()
         #print("\n The true value of std is ",true_value_std)                       #true value of std
         return(true_value_std)
         
@@ -107,7 +109,7 @@ class UserFrame_uuc(Frame):      #UUC Frame
         #==== VARIABLES =====
 
         # Standard Deviation Variables
-        self.uuc_labels = ['UUC-1','UUC-2','UUC-3','UUC-4','UUC-5','UUC-6','UUC-7','UUC-8','UUC-9','UUC-10','Ambient','Ambient correction','Error of DMM']
+        self.uuc_labels = ['UUC-1','UUC-2','UUC-3','UUC-4','UUC-5','UUC-6','UUC-7','UUC-8','UUC-9','UUC-10','AVG. UUC','Ambient','Ambient correction','Error of DMM']
         self.uuc_vars = [DoubleVar()for i in range(len(self.uuc_labels))]
         
         #====================
@@ -119,6 +121,8 @@ class UserFrame_uuc(Frame):      #UUC Frame
         for i, value in enumerate(self.uuc_labels):
             ttk.Label(self, text=value, justify='left', padding=(10, 5, 10, 5)).grid(row=i, column=1, sticky='new')
         for i in range(len(self.uuc_labels)):
+            if i==10:
+                continue
             entry=ttk.Entry(self, textvariable=self.uuc_vars[i])
             entry.grid(row=i, column=2, padx=10, sticky='ew')
             entry.bind('<Return>', self.entry_next)
@@ -126,7 +130,7 @@ class UserFrame_uuc(Frame):      #UUC Frame
             self.data.append(entry)
 
         self.save_btn = ttk.Button(self, text='Save', width=10, command=self.on_save)
-        self.save_btn.grid(row=13, columnspan=3, padx=10, pady=10, sticky='e')
+        self.save_btn.grid(row=14, columnspan=3, padx=10, pady=10, sticky='e')
         self.save_btn.bind('<Return>', self.on_save)
         self.save_btn.bind('<Return>', self.difference,add='+')
     def on_save(self, e=None):
@@ -139,10 +143,10 @@ class UserFrame_uuc(Frame):      #UUC Frame
             sum=sum+self.uuc_vars[i].get()
         avg=sum/10
         print("\n The average is ",round(avg, 2))
-        true_value_uuc_c= avg + self.uuc_vars[11].get() - self.uuc_vars[12].get()
+        true_value_uuc_c= avg + self.uuc_vars[12].get() - self.uuc_vars[13].get()
         print("\n The true value of uuc in celsius is ",round(true_value_uuc_c, 2))   #true value of uuc in celsius
         print("\n The conversion to celsius is ",round(true_value_uuc_c, 2)) 
-        true_value_std=frame_std.on_save()
+        true_value_std=UserFrame_std.on_save()
         final_error = true_value_uuc_c - true_value_std            # [final error=conversion to celsius - true value of std]
         print("\n The final error ",round(final_error, 2))      #final error which is to be displayed in report
 
@@ -157,7 +161,7 @@ class UserFrame_uuc(Frame):      #UUC Frame
     def difference(self,e=None) :
         arr=[DoubleVar()for i in range(10)]
         for i in range(0,10,1):
-            arr[i]=frame_uuc.uuc_vars[i].get() - frame_std.std_vars[i].get()
+            arr[i]=UserFrame_std.uuc_vars[i].get() - UserFrame_std.std_vars[i].get()
         print(arr)
     
     def show_frame(self):
